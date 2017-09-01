5d2a5857630b9ffc7a8883f1e814557a5afd3feb
Bobby Bryant <bobby@Bobbys-MacBook-Pro.local>
2017-09-01 09:04:03 -0400

    create helper function for fetching repeater field settings and normalizing them

--- inc/customizer.php
+++ inc/customizer.php
@@ -15,3 +15,20 @@ function customizer_repeater_sanitize($input){
 	}
 	return $input;
 }
+
+function get_customizer_values( $settings ) {
+	$settings = get_theme_mod( $settings );
+
+	if ( empty( $settings ) ) {
+		return false;
+	}
+
+	return array_map( function( $settings ) {
+		return array_filter( get_object_vars( $settings ), function( $item ) {
+			if ( 'undefined' !== $item ) {
+				return $item;
+			}
+		} );
+	}, json_decode( $settings ) );
+
+}
